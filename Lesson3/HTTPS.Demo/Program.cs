d35627dcf0ba705b70fe035b36ea0ab3de142489
Dimka <godslim1992@gmail.com>
2025-12-30 21:30:07 +0300

    Add Lesson3

---
+++ Lesson3/HTTPS.Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+var client = new HttpClient();
+var post = await client.GetStringAsync("https://jsonplaceholder.typicode.com/posts/1");
+//var posts = await client.GetFromJsonAsync<IEnumerable<Post>>("https://jsonplaceholder.typicode.com/posts");
+//var post = await client.GetFromJsonAsync<Post>("https://jsonplaceholder.typicode.com/posts?id=1");
+Console.WriteLine(post);
+return;
+
+public record Post(
+    [property: JsonPropertyName("userId")] int UserId,
+    [property: JsonPropertyName("id")] int Id,
+    [property: JsonPropertyName("title")] string Title,
+    [property: JsonPropertyName("body")] string Body
+);
